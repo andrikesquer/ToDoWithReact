acb159531e7e9d696882196e1e8064876e035649
andrikesquer <andrikesquer@gmail.com>
2024-09-05 20:05:36 -0600

    Primer uso de componentes con React.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import sum from "./components/sum"; //Imports de librerias, archivos locales, components, estilos, etc.
+
+//Datos, funciones, estructuras, clases, etc.
+
+const num1 = 5;
+const num2 = 4;
+
+// function getRamdom() {
+//   return Math.random();
+// }
+
+// let valor1;
+
+//Declaracion de la funcion del componente
+//Hay dos formas principeles de recibir propiedades, directa o de estructurando
 
 function App() {
+  // Al igual que afuera de la funcion componente, podemos seguir declarando variables, funciones, etc.
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>
+      Resultado: <sum>{sum(num1, num2)}</sum>
+    </p>
   );
 }
 
+// function Header ({ services }) {
+//   // Al igual que afuera de la funcion componente, podemos seguir declarando variables, funciones, etc.
+//   const valor1 = getRamdom();
+//   // HOOKS: Metodos especificos de react para manejar el estado del componente, TIENEN QUE IR DENTRO DE LA FUNCION COMPONENTE
+
+//   // Declaracion de retorno
+//   return {
+//     <>
+//       <Header className="App-header">
+//         Header
+//       <div>
+//       </div>
+
+//     </>
+//   }
+// }
+
+//Exportaciones, comunmente se exporta el componente actual, o podemos exportar multiples componentes, funciones, etc.
 export default App;
